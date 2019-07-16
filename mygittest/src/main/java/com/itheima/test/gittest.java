aa6ad647835d918bbf990dc4de63045e2b5bcc0a
bruceleecf <bruceleecf@163.cn>
2019-07-16 20:17:35 +0800

    随意输出

---
+++ mygittest/src/main/java/com/itheima/test/gittest.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("xioametiousdfjnqwoiejif");
+        System.out.println("xioametiousdfjnqwoiejif");
+        System.out.println("xioametiousdfjnqwoiejif");
+        System.out.println("xioametiousdfjnqwoiejif");
+        System.out.println("xioametiousdfjnqwoiejif");
+    }
+}
